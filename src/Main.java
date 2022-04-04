a5fa39801a9e7cd479e9f978e9bd111d85ee398b
Aftershokx <aftershokx2013@gmail.com>
2022-04-04 18:42:06 +0300

    Working version 1.0

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,20 @@ public class Main {
         Scanner scanner = new Scanner (System.in);
         Parser parser = new Parser ();
         while (true) {
+            System.out.println ("Тестовое задание #1. 1 + 2");
+            System.out.println ("Результат вычисления: ");
+            parser.finalCalculation ("1 + 2");
+            System.out.println ("Тестовое задание #2. 5 - 4 * 2");
+            System.out.println ("Результат вычисления: ");
+            parser.finalCalculation ("5 - 4 * 2");
+            System.out.println ("Тестовое задание #3. 4 + 6 / ( 12 - 3 * 2 )");
+            System.out.println ("Результат вычисления: ");
+            parser.finalCalculation ("4 + 6 / ( 12 - 3 * 2 )");
             System.out.println ("Введите математическое выражение");
             String expression = scanner.nextLine ();
             if (!(expression == null)) {
-                parser.calculate (expression);
-                System.out.println ("Результат вычисления" );
+                System.out.println ("Результат вычисления");
+                parser.finalCalculation (expression);
                 break;
             }else {
                 err ();
